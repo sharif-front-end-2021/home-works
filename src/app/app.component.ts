0e50b3e7f8838cd2921d40dc90c5f51bff4f9168
masharif2012 <isharifpalash@gmail.com>
2021-02-19 18:55:24 -0500

    home work 2 edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
-  title = 'sharif-front-end-app';
+  title = 'The-front-end-application';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeWorkOneComponent } from './home-work-one/home-work-one.component';
 import { HomeWorkTwoComponent } from './home-work-two/home-work-two.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { HomeWorkFiveComponent } from './home-work-five/home-work-five.component';
 
 
 
@@ -11,7 +14,10 @@ import { HomeWorkTwoComponent } from './home-work-two/home-work-two.component';
   declarations: [
     AppComponent,
     HomeWorkOneComponent,
-    HomeWorkTwoComponent
+    HomeWorkTwoComponent,
+    ParentComponent,
+    ChildComponent,
+    HomeWorkFiveComponent
   ],
   
   imports: [
